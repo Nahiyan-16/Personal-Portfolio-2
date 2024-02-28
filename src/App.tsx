11044a6c1a204e639a1db0f976e0837ecc7e6a66
Nahiyan16 <nahiyanahmed11@gmail.com>
2024-02-27 21:57:27 -0500

    Worked on contact page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BackgroundBeamsDemo } from "./components/background-beams";
+import { TextGenerateEffect } from "./components/text-generate-effect";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black">
+      <TextGenerateEffect
+        words={"Hello, My Name is Nahiyan. A web developer"}
+      />
+      <BackgroundBeamsDemo />
     </div>
   );
 }
