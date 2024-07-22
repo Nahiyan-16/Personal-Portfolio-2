f7e807d6c57886280599a85d375fa73d16eadc41
Nahiyan16 <nahiyanahmed11@gmail.com>
2024-07-22 12:17:34 -0400

    Small update

--- src/components/contact-page.tsx
+++ src/components/contact-page.tsx
@@ -34,17 +34,6 @@ const ContactPage: React.FC = () => {
       className="bg-neutral-950 relative flex flex-col items-center justify-center antialiased h-screen"
       id="contact"
     >
-      <div className="w-full absolute h-screen ">
-        <SparklesCore
-          id=""
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className={"`w-full h-full"}
-          particleColor="#FFFFFF"
-        />
-      </div>
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-center font-sans font-bold">
           Contact Me
